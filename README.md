5273e18ca0916550011763494b1932398ea3bf24
GrahamPerich <graham.perich@gmail.com>
2017-01-23 14:53:04 -0800

    add gitignore, update README, use npm package 'prompt' to get user input

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cli-tic-tac-toe
 command line tic tac toe game
+
+### To get started, run `npm install` from the root directory, and then run `node index.js`
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // board constructor
+var prompt = require('prompt');
+prompt.start();
 
 function Game() {
   this.boardMatrix = [];
+
   for (var i = 0; i < 3; i++) {
     this.boardMatrix.push(['__', '__', '__']);
   }
@@ -11,5 +14,21 @@ Game.prototype.showBoard = function() {
   console.log(this.boardMatrix.join('\n'));
 }
 
+Game.prototype.startGame = function() {
+  console.log('player 1 is X');
+  console.log('player 2 is O');
+  console.log('player 1, choose your action:');
+  this.showBoard();
+  prompt.get(['coordinate'], function(err, result) {
+    this.playerAction(result.coordinate)
+  }.bind(this));
+  
+}
+
+Game.prototype.playerAction = function(coordinate) {
+  console.log('PLAYERS COORDINATE CHOSEN: ', coordinate)
+}
+
+
 var myGame = new Game();
-myGame.showBoard();
+myGame.startGame();
